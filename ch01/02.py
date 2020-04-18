1cb72de183beee10735496b71dc848b6bea4721c
Ryutaro Osiro <e175739@ie.u-ryukyu.ac.jp>
2020-04-19 02:09:13 +0900

    finish ch01/02

---
+++ ch01/02.py
@@ -0,0 +1,13 @@
+"""
+03. 「パトカー」＋「タクシー」＝「パタトクカシー」
+「パトカー」＋「タクシー」の文字を先頭から交互に連結して文字列「パタトクカシーー」を得よ．
+"""
+str1 = "パトカー"
+str2 = "タクシー"
+answer = []
+
+for i in range(0,len(str1)):
+    answer.append(str1[i]+str2[i])
+
+answer = "".join(answer)
+print(answer)
